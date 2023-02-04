9e1c39cd5f8610ef48809d16d5306c56301b20d7
mvpetrusenko <70688011+mvpetrusenko@users.noreply.github.com>
2023-02-04 14:10:09 +0100

    Allure report empty

--- src/test/java/tests/UI/SearchProduct/SearchProduct.java
+++ src/test/java/tests/UI/SearchProduct/SearchProduct.java
@@ -1,13 +1,22 @@
 package tests.UI.SearchProduct;
 
 //import java.util.concurrent.TimeUnit;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.junit.Assert;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import tests.UI.base.BaseUITest;
+
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
@@ -45,8 +54,34 @@ public class SearchProduct extends BaseUITest {
         WebDriver driver = new ChromeDriver();
 //                WebElement acceptCookies = driver.findElement(By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]"));
 //
-        WebElement acceptCookies = driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement acceptCookies = driver.findElement(By.xpath("//button[.//text()='Accept Cookies']"));
         acceptCookies.click();
+
+
+//        WebElement acceptCookies = By.xpath("//button[.//text()='Accept Cookies']");
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        wait.until(ExpectedConditions.elementToBeClickable(acceptCookies)).click();
+//
+
+
+
+
+//        WebElement acceptCookies = new WebDriverWait(driver, Duration.ofSeconds(10))
+//                .until(ExpectedConditions.elementToBeClickable(By.xpath("/button[.//text()='Accept Cookies']")));
+//        acceptCookies.click();
+
+
+
+
+
+//        WebElement acceptCookies = driver.findElement(By.cssSelector("input[type='button']"));
+//        acceptCookies.click();
+
+
+
+
 
 
 //        WebElement search = driver.findElement(By.xpath("//a[contains(@href,'/search')]"));
