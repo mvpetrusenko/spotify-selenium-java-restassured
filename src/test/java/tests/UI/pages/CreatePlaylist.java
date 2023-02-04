2887e1645299cc6500ee92f93dfb3d6e953ca576
mvpetrusenko <70688011+mvpetrusenko@users.noreply.github.com>
2023-02-04 14:27:29 +0100

    UI/pages folder was added for Page Object files

---
+++ src/test/java/tests/UI/pages/CreatePlaylist.java
@@ -0,0 +1,4 @@
+package tests.UI.pages;
+
+public class CreatePlaylist {
+}
--- src/test/java/tests/UI/pages/SearchProduct.java
+++ src/test/java/tests/UI/pages/SearchProduct.java
@@ -1,22 +1,15 @@
-package tests.UI.SearchProduct;
+package tests.UI.pages;
 
 //import java.util.concurrent.TimeUnit;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import tests.UI.base.BaseUITest;
 
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
